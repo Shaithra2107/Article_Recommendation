7ce6530a3d19f0754e3efcae23dbfe617e98ea86
Shaithra2107 <shaithrav@gmail.com>
2024-11-14 22:16:16 +0530

    Commit Signup

--- src/main/java/com/example/article/Login.java
+++
@@ -1,5 +0,0 @@
-package com.example.article;
-
-public class Login {
-
-}
---
+++ src/main/java/com/example/article/LoginController.java
@@ -0,0 +1,22 @@
+package com.example.article;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    @FXML
+    public Label messageLabel;
+    @FXML
+    public PasswordField passwordField;
+    @FXML
+    public TextField usernameField;
+
+    public void handleLogin(ActionEvent actionEvent) {
+    }
+
+    public void handleSignUp(ActionEvent actionEvent) {
+    }
+}
